fa3d3a1211606c72de8d33ab69c7703f1a1776f6
Scott910806 <yxma2010@126.com>
2020-06-15 14:30:37 +0800

    use HTMLTestRunner

--- unittest_extend/test_runner.py
+++ unittest_extend/test_runner.py
@@ -1,9 +1,17 @@
+import time
 import unittest
+from HTMLTestRunner import HTMLTestRunner
 
 suit = unittest.defaultTestLoader.discover(
     start_dir = './test_case',
-    pattern = 'parameterized*.py'
+    pattern = 'ddt*.py'
 )
 
-runner = unittest.TextTestRunner()
-runner.run(suit)
+# runner = unittest.TextTestRunner()
+# runner.run(suit)
+
+# 用时间戳区分测试报告
+current_time = time.strftime("%Y_%m_%d_%H_%M_%S")
+with open('./test_reports/'+current_time+' result.html','wb') as f:
+    runner = HTMLTestRunner(stream=f, title='parameterized', description='automation test project')
+    runner.run(suit, rerun=1, save_last_run=False)
